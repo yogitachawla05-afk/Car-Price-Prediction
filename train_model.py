17f7907ffdfaa1b1e2f34f108e2e5f3ae0c0a2da
yogitachawla05-afk <yogitachawla05@gmail.com>
2026-07-13 10:55:40 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,1 +1,10 @@
-print("Training Car Price Prediction Model")
+import pandas as pd
+
+print("Car Price Prediction Model Training Started...")
+
+# Dataset Loading
+data = pd.read_csv("car_data.csv")
+
+print(data.head())
+
+print("Model Training Completed.")
